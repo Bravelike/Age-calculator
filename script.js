0890cb2150bed928583a55b5d8be423f9286ccad
unknown <bunybravelike23@gmail.com>
2023-06-24 07:26:29 +0100

    bug fixed

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ let btnEl = document.getElementById("btn");
 let inputEl = document.getElementById("dob")
 let resultEl = document.getElementById("result")
 
-let birthdayValue = inputEl.value;
 function calculateAge(){
-   
-
+    let birthdayValue = inputEl.value;
+   if(birthdayValue === ""){
+    alert("please enter your birthday")
+   }else{
     let currentDate = new Date();
     let birthday = new Date(birthdayValue);
+    
     let age = currentDate.getFullYear() - birthday.getFullYear();
     let month = currentDate.getMonth() - birthday.getMonth();
     if(month < 0 || (month === 0 && currentDate.getDate() < birthday.getDate())) {
         age--
     }
     resultEl.innerText = `your age is ${age} ${age > 0 ? "years" : "year"} old`
+   }
+
+    
 
 
-    console.log(age)
     return age;
 }
 
 
-btnEl.addEventListener("click", function calculateAge(){
-    if(birthdayValue === ""){
-        alert("please enter your birthday")
-    }
-})
+btnEl.addEventListener("click", calculateAge)
